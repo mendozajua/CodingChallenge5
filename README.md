51e4a0efd9a94387d9df8861676bfe1715ba5cb7
Juan Mendoza <juanmendoza0309@gmail.com>
2021-08-30 12:57:07 -0700

    update readme and finished sorting array

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CodingChallenge5
+
+Description:
+- Write a code to Convert sorted array to balanced binary search tree.
+
+Logical Reasoning:
+
+A watch, which loses time uniformly, was observed to be 5 minutes fast at 8.00 p.m. on Thursday. It was noticed to be 7 minutes slow at 8.00 a.m. on the subsequent Monday. When did the watch show the correct time ? Explanation needed.
+
+a) 7 a.m. Saturday b) 7 a.m. on Friday
+
+c) 10a.m. on Sunday d) 11 a.m. on Friday
--- cc5/src/main/java/com/revature/ArrayToBalancedBinaryTree.java
+++ cc5/src/main/java/com/revature/ArrayToBalancedBinaryTree.java
@@ -1,4 +1,37 @@
 package com.revature;
 
+import java.util.Arrays;
+
 public class ArrayToBalancedBinaryTree {
+
+    public static void main(String[] args){
+        int[] test1 = {5,3,2,8,9};
+        int[] result = sortArray(test1);
+
+       System.out.println(" Sorted: " +  Arrays.toString(result));
+
+
+    }
+    public static int[] sortArray(int[] array){
+        int temp;
+        boolean switcher = false;
+
+
+        while(switcher ==false){
+            switcher = true;
+            for(int i =0; i<array.length-1 ; i++){
+                if(array[i] > array[i+1]){
+                    temp = array[i+1];
+                    array[i+1] = array[i];
+                    array[i] = temp;
+                    switcher = false;
+                }
+            }
+        }
+
+        return array;
+    }
+    public static void convertToBalanceBinaryTree(int[] array){
+
+    }
 }
