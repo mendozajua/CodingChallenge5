937683a90ea2d6d8de2797b24f972fd650b390a8
Juan Mendoza <juanmendoza0309@gmail.com>
2021-09-02 06:45:10 -0700

    finished BST

--- cc5/src/main/java/com/revature/ArrayToBalancedBinaryTree.java
+++ cc5/src/main/java/com/revature/ArrayToBalancedBinaryTree.java
@@ -4,25 +4,30 @@ import java.util.Arrays;
 
 public class ArrayToBalancedBinaryTree {
 
-    public static void main(String[] args){
-        int[] test1 = {5,3,2,8,9};
-        int[] result = sortArray(test1);
+    public static void main(String[] args) {
+        int[] test1 = {5, 3, 2, 8, 9};
+        ArrayToBalancedBinaryTree driver = new ArrayToBalancedBinaryTree();
 
-       System.out.println(" Sorted: " +  Arrays.toString(result));
+        int[] result = driver.sortArray(test1);
 
+        System.out.println(" Sorted: " + Arrays.toString(result));
+        System.out.println("----------------------------");
+        Node testTree = driver.convertToBalanceBinaryTree(result,0, result.length-1);
+        System.out.println(testTree.toString());
 
     }
-    public static int[] sortArray(int[] array){
+
+    public int[] sortArray(int[] array) {
         int temp;
         boolean switcher = false;
 
 
-        while(switcher ==false){
+        while (switcher == false) {
             switcher = true;
-            for(int i =0; i<array.length-1 ; i++){
-                if(array[i] > array[i+1]){
-                    temp = array[i+1];
-                    array[i+1] = array[i];
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i + 1];
+                    array[i + 1] = array[i];
                     array[i] = temp;
                     switcher = false;
                 }
@@ -31,7 +36,39 @@ public class ArrayToBalancedBinaryTree {
 
         return array;
     }
-    public static void convertToBalanceBinaryTree(int[] array){
 
+    public Node convertToBalanceBinaryTree(int[] array, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int middle = (start + end) / 2;
+
+        Node root = new Node(array[middle]);
+
+        root.left = convertToBalanceBinaryTree(array, start, middle-1);
+        root.right = convertToBalanceBinaryTree(array,middle+1, end);
+
+        return root;
+    }
+
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
     }
 }
